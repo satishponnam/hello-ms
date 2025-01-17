7f4ff034d2f6c81ec477c8466a9906ded7051a89
Satish Ponnam <satishkponnam@gmail.com>
2025-01-17 22:58:46 +0530

    Add dev container config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,15 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.0.4"
-	id("io.spring.dependency-management") version "1.1.0"
+	id("org.springframework.boot") version "3.4.1"
+	id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "com.stellify"
-version = "1.0"
-java.sourceCompatibility = JavaVersion.VERSION_17
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
 
-configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
 	}
 }
 
@@ -19,11 +18,9 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
